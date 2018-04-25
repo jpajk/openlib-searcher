ad82f17e568aaa79339ff85b833b5ba659076563
jacek <jpajak@yandex.com>
2018-04-25 17:29:09 +0200

    Animate search

--- src/renderer/components/Search.vue
+++ src/renderer/components/Search.vue
@@ -1,17 +1,41 @@
 <template>
     <div class="row">
-        <div class="col-sm-12">
-            <h3 class="text-center">Search</h3>
+        <div class="col-sm-7 offset-sm-3" :style="{marginTop: marginTop + 'vh'}" id="search-bar" >
+            <h3 class="text-center">Search for books</h3>
+            <form class="">
+                <div class="form-group">
+                    <input @input="handleInput" v-model="searchValue" type="text" class="form-control" id="exampleInputPassword1" placeholder="Type a book title or author in...">
+                </div>
+            </form>
         </div>
     </div>
 </template>
 
 <script>
   export default {
-    name: 'Search'
+    name: 'Search',
+    data: () => {
+      return {
+        searchValue: '',
+        marginTop: 40
+      }
+    },
+    methods: {
+      handleInput (e) {
+        let value = e.target.value
+
+        if (value) {
+          this.marginTop = 10
+        } else {
+          this.marginTop = 40
+        }
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+    #search-bar {
+        transition: margin-top 300ms ease-out;
+    }
 </style>
